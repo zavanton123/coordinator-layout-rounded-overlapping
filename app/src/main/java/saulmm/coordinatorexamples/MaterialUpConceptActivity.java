9b3355d533e03bc51807cc32770544789c18c7d7
zavanton <zavanton@yandex-team.ru>
2020-06-07 11:04:15 +0300

    more - more - mofe - more

--- app/src/main/java/saulmm/coordinatorexamples/MaterialUpConceptActivity.java
+++ app/src/main/java/saulmm/coordinatorexamples/MaterialUpConceptActivity.java
@@ -18,99 +18,102 @@ package saulmm.coordinatorexamples;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.tabs.TabLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import android.view.View;
-import android.widget.ImageView;
 
 public class MaterialUpConceptActivity extends AppCompatActivity
-	implements AppBarLayout.OnOffsetChangedListener {
+    implements AppBarLayout.OnOffsetChangedListener {
 
-	private static final int PERCENTAGE_TO_ANIMATE_AVATAR = 20;
-	private boolean mIsAvatarShown = true;
+    private static final int PERCENTAGE_TO_ANIMATE_AVATAR = 20;
+    private boolean mIsAvatarShown = true;
 
-	private ImageView mProfileImage;
-	private int mMaxScrollSize;
+    private ImageView mProfileImage;
+    private int mMaxScrollSize;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_material_up_concept);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_up_concept);
 
-		TabLayout tabLayout = (TabLayout) findViewById(R.id.materialup_tabs);
-		ViewPager viewPager  = (ViewPager) findViewById(R.id.materialup_viewpager);
-		AppBarLayout appbarLayout = (AppBarLayout) findViewById(R.id.materialup_appbar);
-		mProfileImage = (ImageView) findViewById(R.id.materialup_profile_image);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.materialup_tabs);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.materialup_viewpager);
+        AppBarLayout appbarLayout = (AppBarLayout) findViewById(R.id.materialup_appbar);
+        mProfileImage = (ImageView) findViewById(R.id.materialup_profile_image);
 
-		Toolbar toolbar = (Toolbar) findViewById(R.id.materialup_toolbar);
-		toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-			@Override public void onClick(View v) {
-				onBackPressed();
-			}
-		});
+        Toolbar toolbar = (Toolbar) findViewById(R.id.materialup_toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
 
-		appbarLayout.addOnOffsetChangedListener(this);
-		mMaxScrollSize = appbarLayout.getTotalScrollRange();
+        appbarLayout.addOnOffsetChangedListener(this);
+        mMaxScrollSize = appbarLayout.getTotalScrollRange();
 
-		viewPager.setAdapter(new TabsAdapter(getSupportFragmentManager()));
-		tabLayout.setupWithViewPager(viewPager);
-	}
+        viewPager.setAdapter(new TabsAdapter(getSupportFragmentManager()));
+        tabLayout.setupWithViewPager(viewPager);
+    }
 
-	public static void start(Context c) {
-		c.startActivity(new Intent(c, MaterialUpConceptActivity.class));
-	}
+    public static void start(Context c) {
+        c.startActivity(new Intent(c, MaterialUpConceptActivity.class));
+    }
 
-	@Override
-	public void onOffsetChanged(AppBarLayout appBarLayout, int i) {
-		if (mMaxScrollSize == 0)
-			mMaxScrollSize = appBarLayout.getTotalScrollRange();
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int i) {
+        if (mMaxScrollSize == 0)
+            mMaxScrollSize = appBarLayout.getTotalScrollRange();
 
-		int percentage = (Math.abs(i)) * 100 / mMaxScrollSize;
+        int percentage = (Math.abs(i)) * 100 / mMaxScrollSize;
 
-		if (percentage >= PERCENTAGE_TO_ANIMATE_AVATAR && mIsAvatarShown) {
-			mIsAvatarShown = false;
+        if (percentage >= PERCENTAGE_TO_ANIMATE_AVATAR && mIsAvatarShown) {
+            mIsAvatarShown = false;
 
-			mProfileImage.animate()
-				.scaleY(0).scaleX(0)
-				.setDuration(200)
-				.start();
-		}
+            mProfileImage.animate()
+                .scaleY(0).scaleX(0)
+                .setDuration(200)
+                .start();
+        }
 
-		if (percentage <= PERCENTAGE_TO_ANIMATE_AVATAR && !mIsAvatarShown) {
-			mIsAvatarShown = true;
+        if (percentage <= PERCENTAGE_TO_ANIMATE_AVATAR && !mIsAvatarShown) {
+            mIsAvatarShown = true;
 
-			mProfileImage.animate()
-				.scaleY(1).scaleX(1)
-				.start();
-		}
-	}
+            mProfileImage.animate()
+                .scaleY(1).scaleX(1)
+                .start();
+        }
+    }
 
-	private static class TabsAdapter extends FragmentPagerAdapter {
-		private static final int TAB_COUNT = 2;
+    private static class TabsAdapter extends FragmentPagerAdapter {
+        private static final int TAB_COUNT = 2;
 
-		TabsAdapter(FragmentManager fm) {
-			super(fm);
-		}
+        TabsAdapter(FragmentManager fm) {
+            super(fm);
+        }
 
-		@Override
-		public int getCount() {
-			return TAB_COUNT;
-		}
+        @Override
+        public int getCount() {
+            return TAB_COUNT;
+        }
 
-		@Override
-		public Fragment getItem(int i) {
-			return FakePageFragment.newInstance();
-		}
+        @Override
+        public Fragment getItem(int i) {
+            return FakePageFragment.newInstance();
+        }
 
-		@Override
-		public CharSequence getPageTitle(int position) {
-			return "Tab " + String.valueOf(position);
-		}
-	}
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Tab " + String.valueOf(position);
+        }
+    }
 }
